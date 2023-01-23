fee0ba2bd9d25eea834b80b1b0587107747c6c0f
Princerin <erosnick@gmail.com>
2023-01-24 01:15:38 +0800

    Fixed a update issue(KillTimer is called in the wrong place).

--- Src/6.MultithreadRendering.cpp
+++ Src/6.MultithreadRendering.cpp
@@ -16,8 +16,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	Console console;
-	console.outputDebugStringCapture();
+	//Console console;
+	//console.outputDebugStringCapture();
 
 	try
 	{
