f2c2ae798bb3fb6cee5cc8d9fa6973b2e09e6ab6
Princerin <erosnick@gmail.com>
2023-03-11 22:37:09 +0800

    Update CompileShaders.bat.

--- CompileShaders.bat
+++ CompileShaders.bat
@@ -1,8 +1,8 @@
-%~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T vs_6_0 -E VSMain Assets/Shaders/7.RenderToTexture.hlsl -Fo Assets/Shaders/7.RenderToTextureVS.dxil
-%~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T ps_6_0 -E PSMain Assets/Shaders/7.RenderToTexture.hlsl -Fo Assets/Shaders/7.RenderToTexturePS.dxil
+%~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T vs_6_0 -E VSMain Assets/Shaders/7.RenderToTexture.hlsl -Fo Assets/Shaders/RenderToTextureVS.dxil
+%~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T ps_6_0 -E PSMain Assets/Shaders/7.RenderToTexture.hlsl -Fo Assets/Shaders/RenderToTexturePS.dxil
 
 %~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T vs_6_0 -E VSMain Assets/Shaders/7.RenderToTexture.hlsl -Fo Assets/Shaders/LandAndOcean.dxil
 %~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T ps_6_0 -E PSMain Assets/Shaders/7.RenderToTexture.hlsl -Fo Assets/Shaders/LandAndOcean.dxil
 
-%~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T vs_6_0 -E VSMain Assets/Shaders/7.Skybox.hlsl -Fo Assets/Shaders/7.SkyboxVS.dxil
-%~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T ps_6_0 -E PSMain Assets/Shaders/7.Skybox.hlsl -Fo Assets/Shaders/7.SkyboxPS.dxil
+%~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T vs_6_0 -E VSMain Assets/Shaders/7.Skybox.hlsl -Fo Assets/Shaders/SkyboxVS.dxil
+%~dp0/Tools/dxc_2022_12_16/bin/x64/dxc.exe -T ps_6_0 -E PSMain Assets/Shaders/7.Skybox.hlsl -Fo Assets/Shaders/SkyboxPS.dxil
